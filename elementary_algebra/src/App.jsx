cbd8ebe361f7273c4b90b6020af052da6efda5d9
steve <stephenmarri7@gmail.com>
2025-01-28 07:37:40 -0500

    changed map

--- elementary_algebra/src/App.jsx
+++ elementary_algebra/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   const performAction = action => {
     let result = 0
+    action = String(action).toLowerCase()
     if (action == "add") result = parseFloat(inputOne) + parseFloat(inputTwo)
     if (action == "sub") result = parseFloat(inputOne) - parseFloat(inputTwo)
     if (action == "mul") result = parseFloat(inputOne) * parseFloat(inputTwo)
@@ -14,26 +15,19 @@ function App() {
     setoutputValue(result.toFixed(2))
   }
 
-  const clear = () => {
-    setinputOne(0)
-    setinputTwo(0)
-    setoutputValue(0)
-  }
-
+  const clear = () => {setinputOne(0);setinputTwo(0);setoutputValue(0)}
 
   return (
     <main>
-      <h1 className="text-3xl text-center">Elementary Algebra</h1>
+      <h1 className="text-3xl text-center text-blue-900 font-semibold">Elementary Algebra</h1>
       <div id="output">{outputValue}</div>
       <div id="input" className="fr">
         <input onChange={(e) => setinputOne(e.target.value)} type="number" name="input_1" id="input_1" value={inputOne} />
         <input onChange={(e) => setinputTwo(e.target.value)} type="number" name="input_2" id="input_2" value={inputTwo} />
       </div>
       <div id="buttons" className="fr">
-        <button onClick={() => performAction("add")}>Add</button>
-        <button onClick={() => performAction("sub")}>Sub</button>
-        <button onClick={() => performAction("mul")}>Mul</button>
-        <button onClick={() => performAction("div")}>Div</button>
+        {["Add", "Sub", "Mul", "Div"].
+          map(x => <button key={x} onClick={() => performAction(x)}>{x}</button>)}
       </div>
       <div className="fr">
         <button onClick={()=>clear()} id="clear">Clear</button>
