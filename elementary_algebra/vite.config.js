fecd38a5d26e5dc141f576656796172b5e37fb6a
steve <stephenmarri7@gmail.com>
2025-01-28 07:39:28 -0500

    changed basename

--- elementary_algebra/vite.config.js
+++ elementary_algebra/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "SimpleOnes/elementary_algebra/dist",
+  base: "SimpleOnes/elementary_algebra/dist",
   plugins: [
     react(),
     tailwindcss(),
